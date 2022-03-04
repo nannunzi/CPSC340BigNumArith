b77213e6ab2ef18c938ec950cd533eb49d1ade00
GregSMann <gmann2@mail.umw.edu>
2022-03-04 14:28:39 -0500

    t

--- BigNumArithmetic/src/BigNumArithmetic.java
+++ BigNumArithmetic/src/BigNumArithmetic.java
@@ -9,7 +9,8 @@ public class BigNumArithmetic {
     public static void main(String[] args) throws IOException {
         File source = new File(args[0]);
         LStack string1 = new LStack();
-        ArrayList stringArray=new ArrayList();
+        ArrayList<String> stringArray = new ArrayList();
+        int topLength=0;
 
         if (source.exists()) {
             //if It exists, opens up a file reader
@@ -18,8 +19,8 @@ public class BigNumArithmetic {
             char ch;
             while ((ch = (char) reader.read()) >= 0) {
                 //intended to catch/handle linebreaks, isn't working properly at the moment been working on this for the past 6 hours
-                while (ch!= '\n') {
-
+                while (ch != '\n') {
+                topLength=0;
                     if (ch == ' ') {
                         //Catches spaces, when it finds a whitespace it "locks in" the previous value, and utilizing a simple counter variable, saves it to value 1 (corresponding to red) 2 (corresponding to green) or 3(corresponding to Blue)
                         String value = sb.toString();
@@ -28,6 +29,15 @@ public class BigNumArithmetic {
                     }
 
                 }
+
+                    for (int counter = 0; counter < stringArray.size(); counter++) {
+                        String value=stringArray.get(counter);
+                        if (value.length()>topLength){
+                            topLength=value.length();
+                        }
+
+                }
             }
         }
     }
+}
